3e782e8a533b44209b0efe2693b2d3d67e2bef02
Tufan <66652016+Itsbiggertheinside@users.noreply.github.com>
2021-01-17 11:35:08 +0300

    installed some useful library

--- digitalgameshop/game/admin.py
+++ digitalgameshop/game/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Game
 
 # Register your models here.
+@admin.register(Game)
+class GameAdmin(admin.ModelAdmin):
+    
+    list_display = ('name', 'price', 'developer', 'release_date', 'on_sale', 'pre_order',)
+    list_editable = ('on_sale', 'pre_order', )
+    list_per_page = 15
+
+    
