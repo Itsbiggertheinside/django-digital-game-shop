68895d471bcf8aecb9cf7c166e97ba55216ae9de
Tufan <66652016+Itsbiggertheinside@users.noreply.github.com>
2021-01-19 07:58:00 +0300

    started views

--- digitalgameshop/apps/game/views.py
+++ digitalgameshop/apps/game/views.py
@@ -1,15 +1,19 @@
 from django.shortcuts import render
 from django.views.generic import ListView
 from .models import Game
+from .helpers import GameListHelper
 
 # Create your views here.
 class HomeView(ListView):
-    queryset = Game.objects.select_related('user').prefetch_related('genres', 'languages', 'platform').all()
+    game_lister = GameListHelper()
+    queryset = game_lister.queryset
     template_name = 'base/home.html'
 
         
     def get_context_data(self, **kwargs):
         context = super(HomeView, self).get_context_data(**kwargs)
-        context['queryset'] = self.queryset
+        # context['top_games'] = self.game_lister.get_top_games()
+        context['latest_released_games'] = self.game_lister.get_latest_released_games()
+        context['pre_ordered_games'] = self.game_lister.get_pre_ordered_games()
         return context
     
